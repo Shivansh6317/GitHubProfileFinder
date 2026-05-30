5dabe1642ec31e4c97d7a37831fa3615dc5af903
Shivansh6317 <shivanxhskumar@gmail.com>
2026-05-30 21:29:48 +0530

    initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+function Home(){
+    return(
+        <div>
+            <h1>GitHub Profile Finder</h1>
+            <p>Search for GitHub users and view their profiles.</p>
+        </div>
+    )
+}
+export default Home;
